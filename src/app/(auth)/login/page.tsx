2bacf2407246113e393c5dfce214c47c749e0c72
Mahdy Mubasyir <m.mubasyir19@gmail.com>
2025-08-28 21:45:55 +0700

    add: hooks login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,16 +1,48 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 export default function LoginPage() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmitLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      console.log("form login = ", formData);
+      setFormData({
+        username: "",
+        password: "",
+      });
+    } catch (error) {
+      setError((error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="h-fit w-full rounded-2xl bg-white/30 p-6 backdrop-blur-md md:w-1/4">
+    <div className="h-fit w-full rounded-2xl bg-white/30 p-6 backdrop-blur-md md:w-1/2">
       <div className="text-center">
         <h1 className="text-3xl font-bold text-white">💰 Budget Tracker</h1>
         <p className="mt-2 text-sm font-medium text-slate-300">
           Kelola keuangan Anda dengan mudah dan efektif
         </p>
       </div>
-      <form className="my-8 space-y-4">
+      <form onSubmit={handleSubmitLogin} className="my-8 space-y-4">
         <div id="group-input">
           <label htmlFor="" className="text-sm font-medium text-white">
             Username
@@ -19,6 +51,8 @@ export default function LoginPage() {
             type="text"
             id="username"
             name="username"
+            value={formData.username}
+            onChange={handleChange}
             placeholder="jhon_doe"
             className="mt-1.5 w-full border-b border-gray-300 py-1.5 text-white duration-100 placeholder:text-gray-300 focus:border-white focus:outline-none"
           />
@@ -31,12 +65,18 @@ export default function LoginPage() {
             type="password"
             id="password"
             name="password"
+            value={formData.password}
+            onChange={handleChange}
             className="mt-1.5 w-full border-b border-gray-300 py-1.5 text-white duration-100 placeholder:text-gray-300 focus:border-white focus:outline-none"
           />
         </div>
+        {error && <p className="text-sm text-red-500">{error}</p>}
         <div className="">
-          <button className="w-full cursor-pointer rounded-md bg-green-500 py-2 text-center text-sm font-semibold text-white duration-200 hover:bg-green-600">
-            Login
+          <button
+            type="submit"
+            className="w-full cursor-pointer rounded-md bg-green-500 py-2 text-center text-sm font-semibold text-white duration-200 hover:bg-green-600"
+          >
+            {loading ? "Loading ..." : "Login"}
           </button>
         </div>
       </form>
